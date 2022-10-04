38ec60fbb21f9d274f949de39f3a92a88e5a7135
David Brookton <19512437+brookton@users.noreply.github.com>
2022-10-03 19:40:46 -0600

    :sparkles: styling improvements

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from "next/app";
+import {Session} from "next-auth";
+import {SessionProvider} from "next-auth/react";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({
+  Component,
+  pageProps,
+}: AppProps<{
+  session: Session;
+}>) => {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <div className="app">
+        <Component {...pageProps} />
+      </div>
+    </SessionProvider>
+  );
+};
 
-export default MyApp
+export default MyApp;
